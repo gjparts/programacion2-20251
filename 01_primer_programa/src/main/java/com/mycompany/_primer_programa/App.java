e6d8be25fb32ee9d5fae96cf607290ac37f0072d
gjparts <gjparts@gmail.com>
2025-01-22 11:53:39 -0600

    commit 2201202527709

---
+++ 01_primer_programa/src/main/java/com/mycompany/_primer_programa/App.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._primer_programa;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
