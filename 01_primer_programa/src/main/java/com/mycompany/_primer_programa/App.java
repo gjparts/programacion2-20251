b217735349c7affb1bbbdb5dc39807468ec59415
gjparts <gjparts@gmail.com>
2025-01-23 10:59:32 -0600

    commit 230120254336

--- 01_primer_programa/src/main/java/com/mycompany/_primer_programa/App.java
+++ 01_primer_programa/src/main/java/com/mycompany/_primer_programa/App.java
@@ -11,7 +11,44 @@ package com.mycompany._primer_programa;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //println = imprime y al final pasa a la siguiente linea
+        //print = imprime sin pasar a la siguiente linea
+        System.out.println("Hola UNAH");
+        System.out.println("Gerardo Portillo");
+        System.out.print("Ing. en Sistemas");
+        System.out.println("UNAH Cortes");
+        System.out.print("Rojo");
+        System.out.print("Verde");
+        System.out.print("Azul");
+        
+        //comentario de una sola linea
+        
+        /*bloque de
+        comentarios
+        de varias lineas
+        */
+        //tambien puede usar caracteres de escape en los String a imprimir
+        // \n donde lo coloque pasa a una nueva linea
+        System.out.println("La programacion\nes solo una\nparte de la carrera");
+        // \t donde lo coloque hace una tabulacion
+        System.out.println("Nombre\tEdad\tGenero");
+        System.out.println("Gerardo\t42\tMasculino");
+        System.out.println("Irene\t41\tFemenino");
+        System.out.println("Viena\t16\tFemenino");
+        System.out.println("Josue\t12\tMasculino");
+        // \\ permite imprimir el slash inverso
+        System.out.println("Esto es un slash inverso o contra pleca: \\");
+        // \" permite imprimir una comilla doble dentro de un String
+        System.out.println("Esto es una comilla doble: \"");
+        //imprimir nada mas un salto de linea
+        //forma 1:
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Hoy esta lloviendo");
+        //forma 2:
+        System.out.println("\n\n\n\nHoy esta lloviendo");
     }
 }
 
